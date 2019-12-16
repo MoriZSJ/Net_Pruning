f45df96a7a0248cd5ce0550b3c6c4cc5bd418ecd
PrinceSJ <sj86033503@163.com>
2019-12-16 10:58:23 +0800

    playing around with image transfer

--- img_trans.py
+++ img_trans.py
@@ -1,17 +1,19 @@
 import cv2
 import os
+import sys
 from tqdm import tqdm
 
-oldpath='/home/mori/Programming/Net_Pruning/densenet-pytorch-master/0770/unet/ok&&NG/20190308bmp0010'
+oldpath='/home/mori/Programming/unet++_official/input/circuit/masks'
 newpath=oldpath+'_new/'
 
 os.chdir(oldpath)
 if not os.path.exists(newpath):
     os.makedirs(newpath)
 for img in tqdm(os.listdir()):
-    if img.endswith('bmp'):
+    if img.endswith('jpg'):
         try:
             i=cv2.imread(img)
+            img=img.replace('.jpg','.png')
             o=cv2.imwrite(newpath+img,i)
         except:
             print(img)
